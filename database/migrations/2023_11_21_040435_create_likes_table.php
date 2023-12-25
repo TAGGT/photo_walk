40408782d8cd0d892d159c65c9c522e6fb2942b7
TAGGT <119270185+TAGGT@users.noreply.github.com>
2023-12-25 19:23:57 +0900

    modify likes migrate file

--- database/migrations/2023_11_21_040435_create_likes_table.php
+++ database/migrations/2023_11_21_040435_create_likes_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('likes', function (Blueprint $table) {
+            $table->id();
             $table->foreignId('user_id');
             $table->foreignId('photo_id');
         });
