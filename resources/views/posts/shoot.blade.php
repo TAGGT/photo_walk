885a0077045f946efc1f97143963e32448872fee
TAGGT <119270185+TAGGT@users.noreply.github.com>
2023-12-27 08:55:16 +0900

    modify shoot

--- resources/views/posts/shoot.blade.php
+++ resources/views/posts/shoot.blade.php
@@ -21,7 +21,7 @@
     <form action="/posts" id="post-photo" method="post" enctype="multipart/form-data">
       @csrf
       
-      <div class="tag border-gray-400 w-1/3 border-solid border-2 rounded p-3 m-2">
+      <div class="tag border-gray-400 w-2/3 border-solid border-2 rounded p-3 m-2">
         <h2>Category</h2>
         <select name="post[tag_id]">
           @foreach($tags as $tag)
@@ -39,13 +39,13 @@
         <input type="file" name="image" capture="user" accept="image/*">
       </div>
       
-      <div class="geo-options  border-gray-400 w-1/3 border-solid border-2 rounded p-3 m-2">
+      <div class="geo-options  border-gray-400 w-2/3 border-solid border-2 rounded p-3 m-2">
       <p>県</p>
       <select id="geoapi-prefectures"></select>
         <button class='decide-button' id='prefecture' type='button'>決定</button>
       </div>
 
-      <div class="geo-options border-gray-400 w-1/3 border-solid border-2 rounded p-3 m-2">
+      <div class="geo-options border-gray-400 w-2/3 border-solid border-2 rounded p-3 m-2">
         <p>市区町村</p>
         <select id="geoapi-cities" name="geoapi-cities"></select>
         <p>市区町村一文字目(ひらがな)<br>
@@ -53,7 +53,7 @@
         <button class='decide-button' id='city' type='button'>決定</button></p>
       </div>
 
-      <div class="geo-options  border-gray-400 w-1/3 border-solid border-2 rounded p-3 m-2">
+      <div class="geo-options  border-gray-400 w-2/3 border-solid border-2 rounded p-3 m-2">
         <p>町域</p>
         <select id="geoapi-towns">
         </select>
@@ -61,7 +61,7 @@
       </div>
 
       <!-- 緯度経度入力欄-->
-      <div class=" border-gray-400 w-1/3 border-solid border-2 rounded p-3 m-2">
+      <div class=" border-gray-400 w-2/3 border-solid border-2 rounded p-3 m-2">
       <p>緯度<br>
       <input type="text" name="post[latitude]" id="latitude_form"></p>
       <p class="latitude__error" style="color:red">{{ $errors->first('post.latitude') }}</p>
